563157ebd97f8a735a412208f5185bf73d4620d9
GeriBP <gbelenguer@gmail.com>
2017-07-03 19:23:02 +0200

    Merge

--- hbrsProject/Assets/scripts/CheckPoint.cs
+++ hbrsProject/Assets/scripts/CheckPoint.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour {
     private MenuHandler menuScript;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Interact Key display GO")]
+    public GameObject interactSprite;
+    // Use this for initialization
+    void Start () {
+        menuScript = GameObject.Find("GameMenus").GetComponent<MenuHandler>();
+    }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            interactSprite.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            interactSprite.SetActive(false);
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        {
+            menuScript.UpgradeOpen();
+        }
+    }
     //On trigger enter display E on Players
 
     //On trigger stay and E open upgrade
