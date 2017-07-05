2efc4cde3caabd1c3c9120d1a7389e1550cc3581
GeriBP <gbelenguer@gmail.com>
2017-07-05 17:43:39 +0200

    merge

---
+++ hbrsProject/Assets/scripts/Grenade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour {
+    [SerializeField]
+    float bulletDamage;
+
+    List<GameObject> goList;
+
+    void Start()
+    {
+        goList = new List<GameObject>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!goList.Contains(collision.gameObject))
+        {
+            Debug.Log("Greande in: " + Time.time);
+            goList.Add(collision.gameObject);
+            Entity entity = collision.GetComponent("Entity") as Entity;
+            if (entity == null) return;
+
+            entity.AdjustHealth(-bulletDamage);
+        }
+    }
+}
