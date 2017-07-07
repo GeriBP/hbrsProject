f519360deaaf524199b758a7a5c513a7fdae5527
GeriBP <gbelenguer@gmail.com>
2017-07-08 01:51:32 +0200

    Made a main menu that looks like shit. Started a more promising one that simulates the inside of the sleeping pod or whatever We can use it for narration too

---
+++ hbrsProject/Assets/scripts/MenuEnemies.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuEnemies : MonoBehaviour {
+    public float distance;
+    private bool pos1Bool = true;
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(changeSide());
+    }
+
+    IEnumerator changeSide() {
+        yield return new WaitForSeconds(Random.Range(1.0f, 4.0f));
+        if (pos1Bool)
+        {
+            transform.position = new Vector3(distance, transform.position.y, transform.position.z);
+            pos1Bool = false;
+        }
+        else if (!pos1Bool)
+        {
+            transform.position = new Vector3(-distance, transform.position.y, transform.position.z);
+            pos1Bool = true;
+        }
+        StartCoroutine(changeSide());
+    }
+}
