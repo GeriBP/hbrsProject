c288cec781e3a38a882421666e30317c0df144fd
GeriBP <gbelenguer@gmail.com>
2017-07-09 13:41:37 +0200

    Main menu almost done

---
+++ hbrsProject/Assets/scripts/MessagesMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MessagesMenu : MonoBehaviour {
+    [SerializeField]
+    string[] messages;
+    [SerializeField]
+    Text DisplayText;
+    private int currMessage = 0;
+    private bool canPress = false;
+    // Use this for initialization
+    void Start () {
+        DisplayText.text = messages[currMessage];
+        Invoke("enablePress", 5.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey && canPress)
+        {
+            canPress = false;
+            Invoke("enablePress", 1.0f);
+            ++currMessage;
+            DisplayText.text = messages[currMessage];
+            if(currMessage == messages.Length - 1)
+            {
+                CancelInvoke("enablePress");
+                //Go black
+                //Load next scene
+            }
+        }
+	}
+
+    void enablePress()
+    {
+        canPress = true;
+    }
+}
